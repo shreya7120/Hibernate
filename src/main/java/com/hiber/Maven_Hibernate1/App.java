cd1a8847b2b6e5c2eee9c1773b39d26e8238c7a9
user <user@DESKTOP-TR4LISM>
2024-04-09 13:04:55 +0530

    Hibernate project 1

---
+++ src/main/java/com/hiber/Maven_Hibernate1/App.java
@@ -0,0 +1,12 @@
+package com.hiber.Maven_Hibernate1;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "project started..." );
+        
+      
+    }
+}
